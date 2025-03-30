e505f625d2b986c78d4d6197943a044eef5bbed7
Kavin Kumaran <gk.kavin05@gmail.com>
2025-03-30 15:39:53 -0600

    Add files via upload

---
+++ temp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int choice;
+    float temp;
+
+    printf("Welcome to a very stoopid temperature converter\n")
+    printf("1: Celsius to Farenheit");
+    printf("1: Farenheit to Celsius");
+    printf("1: Kelvin to Celsius and Farenfheit");
+}
