15c0f974b1dc60299f98cba94d1fb389861c0e20
Kavin Kumaran <gk.kavin05@gmail.com>
2025-04-07 19:18:47 -0600

    Add files via upload

---
+++ fun.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "I enjoyed showing upto every class as an excuse consume a liter of black coffee and a 180mg of caffeine energy drink\n";
+    cout << "I enjoyed learning the more low level functions of computers\n";
+    cout << "I also enjoy the fact i no longer get clowned for not knowning C & C++\n";
+    return 0;
+}
